bcd468ecf886a432cf78c4710bdcc43ca86a6dd0
Lucas Coelho Barros <lucasbarrosprofessional@gmail.com>
2021-10-27 18:09:46 -0300

    Criado um jogo simples

---
+++ projeto_jogo_jumping_blocks/functions.js
@@ -0,0 +1,27 @@
+var character = document.querySelector('div#character');
+var ball = document.querySelector('div#ball');
+var counter = 1;
+var score = document.querySelector('label#score');
+
+function jump(){
+    //Counting gamer score
+    score.innerText = `${counter++}`;
+
+    if(character.classList != "animate") {
+        character.classList.add("animate");
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    }, 500);
+}
+
+var checkDead = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var ballLeft = parseInt(window.getComputedStyle(ball).getPropertyValue("left"));
+
+    if((ballLeft > 0 && ballLeft < 20) && (characterTop >= 80)){
+        ball.style.animation = "none";
+        ball.style.display = "none";
+        window.alert('Você perdeu!');
+    }
+}, 10);
